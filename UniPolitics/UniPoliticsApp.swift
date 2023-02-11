42ecaf3b025fba9df5d4b2b16cad88aa68cee809
Sarah <Akhtar>
2023-02-11 14:17:15 -0800

    Initial Commit

---
+++ UniPolitics/UniPoliticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniPoliticsApp.swift
+//  UniPolitics
+//
+//  Created by Sarah Akhtar on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct UniPoliticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
